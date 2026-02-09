d97e4a267f723c4df419fdbe81864435a9568f10
ericlee <eric.l@metis.io>
2026-02-09 15:20:59 +0800

    Update deps and logs

--- main.go
+++ main.go
@@ -2,14 +2,18 @@ package main
 
 import (
 	"context"
+	"encoding/json"
+	"errors"
 	"flag"
 	"fmt"
 	"os"
 	"os/signal"
+	"time"
 
 	"cosmossdk.io/client/v2/autocli"
 	"cosmossdk.io/depinject"
 	"cosmossdk.io/log"
+	coretypes "github.com/cometbft/cometbft/rpc/core/types"
 	"github.com/cosmos/cosmos-sdk/client"
 	"github.com/cosmos/cosmos-sdk/codec"
 	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
@@ -17,15 +21,16 @@ import (
 	"github.com/cosmos/cosmos-sdk/x/auth/tx"
 	"github.com/cosmos/cosmos-sdk/x/auth/types"
 	"github.com/goatnetwork/goat/app"
-	"github.com/goatnetwork/goat/cmd/goatd/cmd"
 )
 
 func main() {
 	var (
 		NodeURL string
+		Start   int64
 	)
 
 	flag.StringVar(&NodeURL, "node", "tcp://localhost:26657", "The node to connect to")
+	flag.Int64Var(&Start, "start", 11044000, "the start height")
 	flag.Parse()
 
 	var (
@@ -40,7 +45,7 @@ func main() {
 	if err := depinject.Inject(
 		depinject.Configs(app.AppConfig(),
 			depinject.Supply(log.NewNopLogger()),
-			depinject.Provide(cmd.ProvideClientContext),
+			depinject.Provide(ProvideClientContext),
 		),
 		&autoCliOpts, &moduleBasicManager, &clientCtx,
 	); err != nil {
@@ -58,33 +63,48 @@ func main() {
 		panic(err)
 	}
 
-	res, err := node.Status(basectx)
+	statusRes, err := node.Status(basectx)
 	if err != nil {
 		panic(err)
 	}
 
-	var list []int64
-	for height := range res.SyncInfo.LatestBlockHeight {
+	fmt.Println("latest block:", statusRes.SyncInfo.LatestBlockHeight)
+
+	var failedList []int64
+	for height := Start; height <= statusRes.SyncInfo.LatestBlockHeight; {
 		if height == 0 {
 			continue
 		}
 
 		fmt.Println("Fetching block:", height)
-		result, err := node.BlockResults(basectx, &height)
+		result, err := func() (res *coretypes.ResultBlockResults, err error) {
+			ctx, cancel := context.WithTimeout(basectx, time.Second*5)
+			defer cancel()
+			return node.BlockResults(ctx, &height)
+		}()
 		if err != nil {
-			panic(err)
+			if errors.Is(err, context.Canceled) {
+				fmt.Println("context canceled, exiting...")
+				return
+			}
+			fmt.Println("failed to fetch block results for height", height, "error:", err)
+			fmt.Println("retrying...")
+			continue
 		}
+
 		for i, txResult := range result.TxsResults {
 			if i != 0 {
 				break
 			}
 
 			if txResult.Code != 0 {
-				list = append(list, height)
+				failedList = append(failedList, height)
+				fmt.Println("got bad block", height)
 			}
 		}
+		height++
 	}
-	fmt.Println("Failed blocks:", list)
+	LogBadBlocks(failedList)
 }
 
 func ProvideClientContext(
@@ -107,4 +127,18 @@ func ProvideClientContext(
 	clientCtx = clientCtx.WithTxConfig(txConfig)
 
 	return clientCtx
+}
+
+func LogBadBlocks(blocks []int64) {
+	if len(blocks) == 0 {
+		fmt.Println("no failed blocks to save")
+		return
+	}
+
+	j := json.NewEncoder(os.Stdout)
+	j.SetIndent("", "  ")
+	if err := j.Encode(blocks); err != nil {
+		panic(err)
+	}
+	fmt.Println(len(blocks), "bad blocks found")
 }
